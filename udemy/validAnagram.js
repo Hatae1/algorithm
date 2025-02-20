997c638e9638ce29d93c886378ac0746efe32123
Hatae1 <ar331@naver.com>
2025-02-20 17:36:22 +0900

    anagramQuestion

---
+++ udemy/validAnagram.js
@@ -0,0 +1,14 @@
+
+
+
+/*
+validAnagram('',''); // true
+validAnagram('aaa', 'zza') // false
+validAnagram('anagram', 'nagaram') // true
+validAnagram('rat', 'car') //false
+validAnagram('awesome', 'awesom') //false
+validAnagram('qwerty', 'qeywrt') //true
+validAnagram('texttwisttime', 'timetwisttext') //true
+
+
+*/
