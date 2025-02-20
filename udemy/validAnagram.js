384f63080f0ce115c881fa68cd2402f4b573d899
Hatae1 <ar331@naver.com>
2025-02-20 22:09:45 +0900

    validAnagram

--- udemy/validAnagram.js
+++ udemy/validAnagram.js
@@ -1,3 +1,39 @@
+function validAnagram(str1, str2){
+
+    if(str1.length !== str2.length){
+        return false;
+    }
+
+    let strArray = {};
+    let strCompareArray = {};
+
+    for(let i=0; i<str1.length; i++){
+        strArray[str1[i]] = (strArray[str1[i]] || 0 ) + 1; 
+    }
+
+    for(let i=0; i<str2.length; i++){
+        strCompareArray[str2[i]] = (strCompareArray[str2[i]] || 0 ) + 1; 
+    }
+
+    for(key in strArray){
+
+        if( strCompareArray[key] === undefined){
+            return false;
+        }
+
+        if(strCompareArray[key] !== strArray[key]){
+            console.log(strCompareArray[key]);
+            return false;
+        }
+    }
+
+    return true;
+    
+    
+}
+
+validAnagram('aaa', 'zza');
+
 
 
 
